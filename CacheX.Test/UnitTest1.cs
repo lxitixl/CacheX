e50cb7272d9496865320172faa7c1d6e213727d7
lxitixl <lxitixl@gmail.com>
2021-10-01 00:17:52 +0100

    init

---
+++ CacheX.Test/UnitTest1.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Internal;
+using Moq;
+using NUnit.Framework;
+
+namespace CacheX.Test
+{
+    public class Tests
+    {
+        private Mock<ISystemClock> _systemClock;
+        private CacheX<string, string> _cacheX;
+
+        [SetUp]
+        public void Setup()
+        {
+            _systemClock = new Mock<ISystemClock>();
+            _cacheX = new(systemClock: _systemClock.Object);
+        }
+
+        [Test]
+        public async Task Cache_Not_Expired()
+        {
+            _systemClock.Setup(x => x.UtcNow).Returns(DateTime.UtcNow);
+            var result1 = await _cacheX.GetOrAdd("key", async () => await Task.FromResult("result1"), TimeSpan.FromMinutes(10));
+            Assert.AreEqual("result1", result1);
+
+            _systemClock.Setup(x => x.UtcNow).Returns(DateTime.UtcNow.AddDays(-1));
+            var result2 = await _cacheX.GetOrAdd("key", async () => await Task.FromResult("result2"), TimeSpan.FromMinutes(10));
+            Assert.AreEqual("result1", result2);
+        }
+
+        [Test]
+        public async Task Cache_Is_Expired()
+        {
+            _systemClock.Setup(x => x.UtcNow).Returns(DateTime.UtcNow);
+            var result1 = await _cacheX.GetOrAdd("key", async () => await Task.FromResult("result1"), TimeSpan.FromMinutes(10));
+            Assert.AreEqual("result1", result1);
+
+            _systemClock.Setup(x => x.UtcNow).Returns(DateTime.UtcNow.AddDays(1));
+            var result2 = await _cacheX.GetOrAdd("key", async () => await Task.FromResult("result2"), TimeSpan.FromMinutes(10));
+            Assert.AreEqual("result2", result2);
+        }
+    }
+}
---
+++ CacheX/CacheX.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Internal;
+
+namespace CacheX
+{
+    public class CacheX<TKey, TValue>
+    {
+        private readonly IMemoryCache _cache;
+        private readonly ConcurrentDictionary<TKey, SemaphoreSlim> _sync;
+        private readonly ISystemClock _systemClock;
+
+        public CacheX(ISystemClock systemClock = null)
+        {
+            _systemClock = systemClock ?? new SystemClock();
+            _cache = new MemoryCache(new MemoryCacheOptions { Clock = _systemClock });
+            _sync = new();
+        }
+
+        public async Task<TValue> GetOrAdd(TKey key, Func<Task<TValue>> createValueFactory)
+        {
+            return await GetOrAdd(key, createValueFactory, default);
+        }
+
+        public async Task<TValue> GetOrAdd(TKey key, Func<Task<TValue>> createValueFactory,
+            TimeSpan absoluteExpiration = default)
+        {
+            if (_cache.TryGetValue(key, out TValue cachedValue))
+                return cachedValue;
+
+            var semaphore = _sync.GetOrAdd(key, x => new SemaphoreSlim(1, 1));
+            await semaphore.WaitAsync();
+            try
+            {
+                if (_cache.TryGetValue(key, out cachedValue))
+                    return cachedValue;
+
+                cachedValue = await createValueFactory();
+
+                return absoluteExpiration == default ?
+                    _cache.Set(key, cachedValue) :
+                    _cache.Set(key, cachedValue, absoluteExpiration);
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        }
+    }
+}
